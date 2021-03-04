7ab3df7eecc48fc853b266429357b0c082875fcb
enditywasita <65750302+enditywasita@users.noreply.github.com>
2021-03-04 15:39:20 +0700

    cek bilangan selain integer

--- tugas1.c
+++ tugas1.c
@@ -3,22 +3,28 @@
 int main(void)
 {
     int x;
-
+    
     printf ("Masukkan sebuah integer : ");
-    scanf ("%i", &x);
-
-    if (x > 0)
+    
+    if (scanf ("%i", &x) != 1)
     {
-        printf ("Angka yang anda masukkan merupakan bilangan positif\n");
-    }
-    else if (x < 0)
-    {
-        printf ("Angka yang anda masukkan merupakan bilangan negatif\n");
+        printf ("Tolong masukkan bilangan integer\n");
     }
     else
     {
-        printf ("Angka yang anda masukkan merupakan 0\n");
+         if (x > 0)
+        {
+            printf ("Angka yang anda masukkan merupakan bilangan positif\n");
+        }
+        else if (x < 0)
+        {
+            printf ("Angka yang anda masukkan merupakan bilangan negatif\n");
+        }
+        else
+        {
+            printf ("Angka yang anda masukkan merupakan 0\n");
+        }
     }
-
+    
     return 0;
 }
