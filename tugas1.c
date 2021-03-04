172630f72f1cf62bba3a0d687453c7529add6a82
enditywasita <65750302+enditywasita@users.noreply.github.com>
2021-03-04 16:01:49 +0700

    tugas1

---
+++ tugas1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+
+    printf ("Masukkan sebuah integer : ");
+    scanf ("%i", &x);
+
+    if (x > 0)
+    {
+        printf ("Angka yang anda masukkan merupakan bilangan positif\n");
+    }
+    else if (x < 0)
+    {
+        printf ("Angka yang anda masukkan merupakan bilangan negatif\n");
+    }
+    else
+    {
+        printf ("Angka yang anda masukkan merupakan 0\n");
+    }
+
+    return 0;
+}
